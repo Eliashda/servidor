754798f188341702a8e71c7ee884464c81022e05
ELIAS ALMENDA RODRIGUES PADILHA <elias_a_padilha@estudante.sesisenai.org.br>
2025-04-02 10:48:01 -0300

    gitignore

--- teste.js
+++ teste.js
@@ -4,9 +4,9 @@ const { soma } = require('./script')
 const PORT = 8000
 
 const server = http.createServer((req,res) => {
-   const urlCapturada= url.parse(req.url, true)
-   console.log(urlCapturada)
-   const {querry} = urlCapturada
+    const urlCapturada= url.parse(req.url, true)
+    console.log(urlCapturada)
+    const {querry} = urlCapturada
     let resultado = soma(Number(querry.a), Number(querry.b))
     res.end(`o resultado ficou em ${resultado}`)
 })
@@ -16,4 +16,3 @@ const server = http.createServer((req,res) => {
 server.listen(PORT,() => {
     console.log(`o server foi iniciado na porta ${PORT}`)
 })
-
